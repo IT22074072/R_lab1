db4a1812c0958a308fabbcb894b75550edb76b97
Dinithi Sanjana <IT22074072@my.sliit.lk>
2024-04-04 12:34:46 +0530

    Operators

--- intro.R
+++ intro.R
@@ -5,3 +5,22 @@ getwd()
 
 #t set the current working directory
 setwd("E:\\Y2S2\\R_Lab")
+
+#help
+?solve
+help("data.frame")
+
+#scaler operators
+2+8
+900-27
+50*6
+50/4
+4^2
+4**4
+4*4
+600%%10
+
+
+#logical operators
+x=5
+y=-4
